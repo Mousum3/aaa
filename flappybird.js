d3b85a993aae0c363267f53ff7c5f927d6422e77
Mousum3 <145050412+Mousum3@users.noreply.github.com>
2024-06-14 21:38:24 +0600

    Update

--- flappybird.js
+++ flappybird.js
@@ -146,7 +146,7 @@ function placePipes() {
 }
 
 function moveBird(e) {
-    if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX" || e.type === "touchstart") {
+    if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX" || e.type === "touchmove") {
         //jump
         velocityY = -6;
 
